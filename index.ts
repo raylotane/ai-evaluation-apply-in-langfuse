4e6ca79b2e0979821bf38609cbd21a897d179512
raylo <raylotane@gmail.com>
2026-05-07 08:17:41 +0800

    feat: 接入AI api

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun 123!");
+import { generateText } from 'ai';
+import { createDeepSeek } from '@ai-sdk/deepseek';
+
+const deepseek = createDeepSeek({
+  apiKey: process.env.DEEPSEEK_API_KEY,
+});
+
+const { text } = await generateText({
+  model: deepseek('deepseek-v4-flash'),
+  prompt: 'hi, how are you?',
+});
+
+console.log(text);
